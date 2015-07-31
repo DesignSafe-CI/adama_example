229d1f1611f7372e43ae5f638b9fcb15fe395432
Walter Moreira <walter@waltermoreira.net>
2015-07-31 15:35:41 -0500

    Return geo coordinates in decimal

--- notebooks/demo/services/common/tools.py
+++ notebooks/demo/services/common/tools.py
@@ -1,8 +1,30 @@
+# -*- coding: utf-8 -*-
+
 import csv
 import os
+import re
 
 
 HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+def sexa_to_dec(dh, min, secs, sign):
+    return sign*(dh + float(min)/60 + float(secs)/60**2)
+
+
+def string_to_dec(s, neg):
+    parsed = filter(
+        None, re.split('[\'" °]', unicode(s, 'utf-8')))
+    sign = -1 if parsed[-1] == neg else 1
+    return sexa_to_dec(float(parsed[0]), float(parsed[1]), float(parsed[2]),
+                       sign)
+
+
+def process_geo_coordinates(obj):
+    if obj['Latitude']:
+        obj['Latitude'] = string_to_dec(obj['Latitude'], 'S')
+    if obj['Longitude']:
+        obj['Longitude'] = string_to_dec(obj['Longitude'], 'W')
 
 
 def load_db():
@@ -10,6 +32,7 @@ def load_db():
         reader = csv.DictReader(f)
         for elt in reader:
             del elt['']
+            process_geo_coordinates(elt)
             yield elt
 
 
